00c41b84f0a989302747a602b4b814b205fc7be9
ikraamgh <128657710+ikraamgh@users.noreply.github.com>
2024-03-11 16:04:02 +0000

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const token = req.headers.authorization;
+
+    // Vérifier si le token est présent
+    if (!token) {
+        return res.status(403).json({ message: "Le token est obligatoire pour l'authentification." });
+    }
+
+    try {
+        // Vérifier le token
+        const decoded = jwt.verify(token, process.env.TOKEN);
+        req.user = decoded; // Stocker les informations de l'utilisateur décodées dans req.user
+        next(); 
+    } catch (error) {
+        return res.status(401).json({ message: "Token invalide." });
+    }
+}
+
+module.exports = {
+    verifyToken
+};  
